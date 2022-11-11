73b5e9b4de93d2a4c15ddeb34d263704242e1aaa
Jan Luiz <jan_2020@Hotmail.com>
2022-11-11 13:02:25 -0300

    Comecando o projeto

---
+++ TabuleiroJogo/Peca.java
@@ -0,0 +1,18 @@
+package TabuleiroJogo;
+
+public class Peca {
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	
+	
+
+}
---
+++ aplicattion/Programa.java
@@ -0,0 +1,13 @@
+package aplicattion;
+
+import TabuleiroJogo.Posicao;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		Posicao posicao = new Posicao(2, 4);
+		System.out.println(posicao);
+	}
+	
+}
